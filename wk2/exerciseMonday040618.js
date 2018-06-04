0700bef020fdfe02aa330cecbe4322a331ede2da
Mario Colindres <mcolindres@gmail.com>
2018-06-04 17:50:09 +0200

    finished ex 1-3

--- wk2/exerciseMonday040618.js
+++ wk2/exerciseMonday040618.js
@@ -1,7 +1,7 @@
 //ex #1
 
 function fn(select) {
-    var str = document.querySelector();
+    var str = document.querySelector(select);
 
     for (var i = 0; i < str.length; i++) {
         str[i].style.fontWeight = "bold";
@@ -9,7 +9,25 @@ function fn(select) {
         str[i].style.textDecoration = "underline";
     }
 }
+fn();
 
 //ex #2
+function fun(x) {
+    var str = document.getElementsByClassName(x);
+    return str;
+}
+fun("content");
 
-var str = document.getElementsByClassName("className");
+// ex #3
+function c() {
+    var elem = document.createElement("div");
+    var textNode = document.createTextNode("AWESOME");
+    elem.style.position = "fixed";
+    elem.style.zindex = 2147483647;
+    elem.style.left = 20 + "px";
+    elem.style.top = 100 + "px";
+    elem.style.fontSize = 200 + "px";
+    elem.appendChild(textNode);
+    document.body.appendChild(elem);
+}
+c();
