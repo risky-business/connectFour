baae4fe951595881697d5ba5179d5c974f2d8ec0
Mario Colindres <mcolindres@gmail.com>
2018-06-11 18:07:37 +0200

    finished pop up screen

--- reichstag_demo/reichstag.js
+++ reichstag_demo/reichstag.js
@@ -2,7 +2,7 @@
     var menuIcon = document.getElementsByClassName("hamburger-menu")[0];
     var menu = document.getElementsByClassName("menu")[0];
     var menuBack = document.getElementById("hamburger-pop-menu");
-    var x = document.getElementsByClassName("x");
+    var x = document.getElementById("x");
 
     menuIcon.addEventListener("click", function() {
         document.body.classList.add("active");
@@ -15,5 +15,17 @@
     });
     x.addEventListener("click", function() {
         document.body.classList.remove("active");
+    });
+
+    setTimeout(function() {
+        $(".greeting-background").css({
+            display: "block"
+        });
+    }, 1000);
+
+    $(".close").on("click", function() {
+        $(".greeting-background").css({
+            display: "none"
+        });
     });
 })();
