71559765ff6d3fd8bab5893863ea3c0b0a7ffaa0
Mario Colindres <mcolindres@gmail.com>
2018-06-17 20:12:46 +0200

    diagonal victory code finished, still need animation

--- connect4Project/script.js
+++ connect4Project/script.js
@@ -1,5 +1,6 @@
 (function() {
     var currentPlayer = "player1";
+    var diaVictory = false;
 
     $(".column").on("click", function(e) {
         var slotsInColumn = $(e.currentTarget).find(".slot");
@@ -12,20 +13,68 @@
         }
         slotsInColumn.eq(i).addClass(currentPlayer);
 
-        if (checkForVictory(slotsInColumn)) {
-            //alert("Vertical victory");
+        if (checkForVictory(slotsInColumn) && diaVictory) {
+            alert("Vertical victory");
         } else {
             if (checkForVictory($(".row" + i))) {
-                //alert("victory");
+                alert("victory");
+            } else {
+                if (diagonalVictory()) {
+                    alert("Victory");
+                }
+            }
+
+            if (currentPlayer == "player1") {
+                currentPlayer = "player2";
+            } else if (currentPlayer == "player2") {
+                currentPlayer = "player1";
             }
         }
-
-        if (currentPlayer == "player1") {
-            currentPlayer = "player2";
-        } else if (currentPlayer == "player2") {
-            currentPlayer = "player1";
-        }
     });
+
+    function diagonalVictory() {
+        var slots = $(".slot");
+        //diagonal possibilites
+        var lists = [
+            [0, 7, 14, 21],
+            [7, 14, 21, 28],
+            [14, 21, 28, 35],
+            [1, 8, 15, 22],
+            [8, 15, 22, 29],
+            [2, 9, 16, 23],
+            [6, 13, 20, 27],
+            [13, 20, 27, 34],
+            [20, 27, 34, 41],
+            [12, 19, 26, 33],
+            [19, 26, 33, 40],
+            [18, 25, 32, 39],
+            [3, 8, 13, 18],
+            [4, 9, 14, 19],
+            [9, 14, 19, 24],
+            [5, 10, 15, 20],
+            [10, 15, 20, 25],
+            [15, 20, 25, 30],
+            [11, 16, 21, 26],
+            [16, 21, 26, 31],
+            [21, 26, 31, 36],
+            [17, 22, 27, 32],
+            [22, 27, 32, 37],
+            [23, 28, 33, 38]
+        ];
+
+        for (var i = 0; i < lists.length; i++) {
+            if (slots.eq(lists[i][0]).hasClass(currentPlayer)) {
+                if (slots.eq(lists[i][1]).hasClass(currentPlayer)) {
+                    if (slots.eq(lists[i][2]).hasClass(currentPlayer)) {
+                        if (slots.eq(lists[i][3]).hasClass(currentPlayer)) {
+                            console.log("victory");
+                            diaVictory = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     function checkForVictory(slots) {
         var str = "";
@@ -43,52 +92,3 @@
         location.reload(); //once game is done
     });
 })();
-
-// // function(){
-// // var col=0;
-// // var row=-1;
-// //
-// // [
-// //     ".column.nth-child(col1) .row:nth-child(row0)",
-// //     ".column.nth-child(col2) .row:nth-child(row0)",
-// //     ".column.nth-child(col3) .row:nth-child(row0)",
-// //     ".column.nth-child(col4) .row:nth-child(row0)",
-// //     ".column.nth-child(col5) .row:nth-child(row0)",
-// //     ".column.nth-child(col6) .row:nth-child(row0)",
-// //     ".column.nth-child(col1) .row:nth-child(row0)"
-// //
-// //
-// // ]
-// // }
-// var victories=[
-//     [0,7,14,21],
-//     [1,8,15,22],
-//     [2,9,16,23],
-//     [3,8,13,18]
-// ]
-//
-//
-//
-//
-// var slots =$('.slot');
-// var list =[0,7,14,21]
-//
-// if (slots.eq(lists [0]).hasClass(currentPlayer)){
-//     if (slots.eq(lits[1]).hasClass(currentPlayer)){
-//
-//     }
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// \
-
-///$('.resetButton').on('click', function(){
-//location.reload(); //once game is done
-// }
